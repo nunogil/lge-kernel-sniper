0cfd1f0ad301668238d5f67da98e389e4318ea54
Nuno Gil <nunogil@outlook.com>
2012-09-11 01:01:38 +0100

    writeback and cache changes for battery life

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -165,7 +165,7 @@ static char * const zone_names[MAX_NR_ZONES] = {
 	 "Movable",
 };
 
-int min_free_kbytes = 1024;
+int min_free_kbytes = 4096;
 int min_free_order_shift = 1;
 
 static unsigned long __meminitdata nr_kernel_pages;
@@ -4992,11 +4992,6 @@ static int __init init_per_zone_wmark_min(void)
 
 	lowmem_kbytes = nr_free_buffer_pages() * (PAGE_SIZE >> 10);
 
-	min_free_kbytes = int_sqrt(lowmem_kbytes * 16);
-	if (min_free_kbytes < 128)
-		min_free_kbytes = 128;
-	if (min_free_kbytes > 65536)
-		min_free_kbytes = 65536;
 	setup_per_zone_wmarks();
 	setup_per_zone_lowmem_reserve();
 	setup_per_zone_inactive_ratio();
